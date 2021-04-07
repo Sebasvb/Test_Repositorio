0a7fe63613cab7237ac688f0f7490c6529aa7607
Sebasvb <sebastian.venero@utec.edu.pe>
2021-04-07 11:45:29 -0500

    holi

---
+++ yolito/range_len.py
@@ -0,0 +1,11 @@
+from cv2 import cv2
+
+def range_lens (boxes,indexes,classes,class_ids,confidences,colors,frame,font):
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 1, color, 3)
